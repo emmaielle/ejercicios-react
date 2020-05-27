9ace42cfd2319699b2f983f11fe9c725ad245c12
Moi <moira.lasserre@gmail.com>
2020-05-27 00:36:59 -0300

    Hogwarts as Pokemons

---
+++ 01-intro-a-react/harry-potter.jsx
@@ -0,0 +1,34 @@
+{
+  pokemon: [
+    {
+      id: 1,
+      name: "Hermione",
+      house: "Gryffindor",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png"
+    },
+    {
+      id: 2,
+      name: "Luna",
+      house: "Ravenclaw",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png"
+    },
+    {
+      id: 3,
+      name: "Draco",
+      house: "Slytherin",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/12.png"
+    },
+    {
+      id: 4,
+      name: "Cho",
+      house: "Hufflepuff",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/19.png"
+    },
+    {
+      id: 5,
+      name: "Ron",
+      house: "Gryffindor",
+      image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/11.png"
+    }
+  ]
+}
