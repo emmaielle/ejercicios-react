c690fc9bb4cb10b2cf93c9220e56b893feb33a40
Moi <moira.lasserre@gmail.com>
2020-06-02 18:57:14 -0300

    added names

---
+++ 02-lifecycles-hooks/nombres.js
@@ -0,0 +1,14 @@
+const nombres = [
+  "Linda Waters",
+  "Nadia Collins",
+  "Micaela Mcdowell",
+  "Carl Johns",
+  "Jean Hayden",
+  "Aliana Estes",
+  "Caitlin Haley",
+  "Emelia Villa",
+  "Issac Middleton",
+  "Abbey Esparza",
+  "Hassan Bowen",
+  "Gilberto Mora",
+]
